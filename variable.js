4c4b64f15db850869d2d04f703316f1fc3ac0815
KAJALIYA CHARCHIL <er.charchilkajaliya@gmail.com>
2021-05-03 09:30:38 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,27 @@
+
+// //Below function has no name
+// let mySum = function(a,b) {
+//     return a+b;
+// }
+
+// let sum1 = mySum(3,4);
+// let sum2 = mySum(5,-4);
+
+// console.log(sum1); //7
+// console.log(sum2); //1
+
+//forEach loop
+// let myArray = new Array;
+// myArray = ['John', 'Bob', 'Tom', 'Sam'];
+
+// myArray.forEach(function(element,index,array) {
+//     console.log(`At index ${index} -> ${element} available`);
+// });
+
+// //using fat arrow function
+// myArray.forEach((element,index,array) => {
+//     console.log(`At index ${index} -> ${element} available`);
+// });
+
+myArray = [1,2,3,4,5];
+myArray.shift()
